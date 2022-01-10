332b1c3a7de0491aaacfce462bfc0dacd98e4072
Мащицкий Игорь <mashchitskiy_ie@taximaxim.ru>
2022-01-10 13:57:33 +0500

    Add project files.

---
+++ Median Filter/MainForm.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace MedianFilter
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBoxIn = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.pictureBoxOut = new System.Windows.Forms.PictureBox();
+            this.TimerTB = new System.Windows.Forms.TextBox();
+            this.edgeCB = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pNCB = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxIn)).BeginInit();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxOut)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBoxIn
+            // 
+            this.pictureBoxIn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.SetColumnSpan(this.pictureBoxIn, 2);
+            this.pictureBoxIn.Location = new System.Drawing.Point(317, 3);
+            this.pictureBoxIn.Name = "pictureBoxIn";
+            this.pictureBoxIn.Size = new System.Drawing.Size(236, 191);
+            this.pictureBoxIn.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBoxIn.TabIndex = 0;
+            this.pictureBoxIn.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.SetColumnSpan(this.button1, 2);
+            this.button1.Location = new System.Drawing.Point(456, 684);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(200, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Load Picture";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 4;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 40.75F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 9.25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 9.25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 40.75F));
+            this.tableLayoutPanel1.Controls.Add(this.pictureBoxOut, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.pictureBoxIn, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.TimerTB, 1, 4);
+            this.tableLayoutPanel1.Controls.Add(this.button1, 1, 3);
+            this.tableLayoutPanel1.Controls.Add(this.edgeCB, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.pNCB, 2, 2);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 2, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 5;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 85.18F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 2.7F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 4.04F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 4.04F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 4.04F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1114, 742);
+            this.tableLayoutPanel1.TabIndex = 2;
+            // 
+            // pictureBoxOut
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.pictureBoxOut, 2);
+            this.pictureBoxOut.Location = new System.Drawing.Point(559, 3);
+            this.pictureBoxOut.Name = "pictureBoxOut";
+            this.pictureBoxOut.Size = new System.Drawing.Size(236, 191);
+            this.pictureBoxOut.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBoxOut.TabIndex = 2;
+            this.pictureBoxOut.TabStop = false;
+            // 
+            // TimerTB
+            // 
+            this.TimerTB.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.TimerTB.BackColor = System.Drawing.SystemColors.Window;
+            this.tableLayoutPanel1.SetColumnSpan(this.TimerTB, 2);
+            this.TimerTB.Location = new System.Drawing.Point(456, 719);
+            this.TimerTB.Name = "TimerTB";
+            this.TimerTB.ReadOnly = true;
+            this.TimerTB.Size = new System.Drawing.Size(200, 20);
+            this.TimerTB.TabIndex = 5;
+            this.TimerTB.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // edgeCB
+            // 
+            this.edgeCB.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.edgeCB.FormattingEnabled = true;
+            this.edgeCB.Items.AddRange(new object[] {
+            "3",
+            "5",
+            "7",
+            "9"});
+            this.edgeCB.Location = new System.Drawing.Point(483, 655);
+            this.edgeCB.Name = "edgeCB";
+            this.edgeCB.Size = new System.Drawing.Size(43, 21);
+            this.edgeCB.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(467, 636);
+            this.label2.Margin = new System.Windows.Forms.Padding(3);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Window Edge";
+            // 
+            // pNCB
+            // 
+            this.pNCB.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.pNCB.FormattingEnabled = true;
+            this.pNCB.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "4",
+            "8"});
+            this.pNCB.Location = new System.Drawing.Point(586, 655);
+            this.pNCB.Name = "pNCB";
+            this.pNCB.Size = new System.Drawing.Size(43, 21);
+            this.pNCB.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(564, 636);
+            this.label1.Margin = new System.Windows.Forms.Padding(3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Processing Units";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1114, 742);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "MainForm";
+            this.Text = "Median Filter";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxIn)).EndInit();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxOut)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBoxIn;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.PictureBox pictureBoxOut;
+        private System.Windows.Forms.ComboBox pNCB;
+        private System.Windows.Forms.ComboBox edgeCB;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox TimerTB;
+    }
+}
+
---
+++ Median Filter/MainForm.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace MedianFilter
+{
+    public partial class MainForm : Form
+    {
+        Thread TimeThread, MainThread;
+        List<Thread> Threads;
+        Bitmap OutPic;
+        Color[,] pixIn, pixOut;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            edgeCB.SelectedIndex = 1;
+            pNCB.SelectedIndex = 1;
+            this.FormClosing += new FormClosingEventHandler((object s, FormClosingEventArgs e) => KillAllThreads());
+        }
+
+        private void KillAllThreads()
+        {
+            if (TimeThread != null) TimeThread.Abort();
+            if (Threads != null) foreach (Thread T in Threads) T.Abort();
+            if (MainThread != null) MainThread.Abort(); 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int edge = Convert.ToInt32(edgeCB.Items[edgeCB.SelectedIndex]), pN = Convert.ToInt32(pNCB.Items[pNCB.SelectedIndex]); 
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK) 
+            {
+                KillAllThreads();
+                pictureBoxIn.Image = new Bitmap(openFileDialog1.FileName);
+                MainThread = new Thread(() =>
+                {
+                    TimeThread = new Thread(() =>
+                    {
+                        Stopwatch t = new Stopwatch();
+                        t.Start();
+                        while (MainThread.IsAlive) Invoke(new Action(() => TimerTB.Text = string.Format("{0:00}:{1:00}:{2:00}", t.Elapsed.Minutes, t.Elapsed.Seconds, t.Elapsed.Milliseconds / 10)));
+                    });
+                    TimeThread.Start();
+                    OutPic = new Bitmap(openFileDialog1.FileName);
+                    pixIn = new Color[OutPic.Height, OutPic.Width];
+                    pixOut = new Color[OutPic.Height, OutPic.Width];
+                    for (int i = 0; i < pixIn.GetLength(0); i++) for (int j = 0; j < pixIn.GetLength(1); j++) pixIn[i, j] = OutPic.GetPixel(j, i);
+                    MedianFilterParallel(edge, pN);
+                });
+                MainThread.Start();
+            }
+        }
+
+        public Bitmap MedianFilter(Bitmap pic, int edge)
+        {
+            int W = pic.Width, H = pic.Height, CellLen = edge * edge;
+            int m = CellLen / 2, n = edge / 2;
+            for (int i = n; i < H - n; i++)
+            {
+                for (int j = n; j < W - n; j++)
+                {
+                    int[] R = new int[CellLen], G = new int[CellLen], B = new int[CellLen];
+                    for (int k = i - n; k <= i + n; k++) for (int l = j - n; l <= j + n; l++) { Color clr = pic.GetPixel(k, l); int p = (k - i + n) * edge + l - j + n; R[p] = clr.R; G[p] = clr.G; B[p] = clr.B; }
+                    Array.Sort(R); Array.Sort(G); Array.Sort(B);
+                    pic.SetPixel(i, j, Color.FromArgb(R[m], G[m], B[m]));
+                }
+            }
+            return pic;
+        }
+
+        private void MedianFilterParallel(int edge, int pN)
+        {
+
+            Threads = new List<Thread>();
+            int HP = 1, VP = 1, n = edge - 1;
+            switch (pN)
+            {
+                case 2: { VP = 2; break; }
+                case 4: { HP = 2; VP = 2; break; }
+                case 8: { HP = 2; VP = 4; break; }
+            }
+            int H = (OutPic.Height - n) / VP, W = (OutPic.Width - n) / HP;
+            n /= 2;
+            for (int i = 0; i < VP; i++)
+            {
+                for (int j = 0; j < HP; j++)
+                {
+                    int stRow = n + i * H, stCol = n + j * W, endRow = stRow + H - 1, endCol = stCol + W - 1;
+                    Thread T = new Thread(() => MedianFilterOnRectangle(edge, stRow, stCol, endRow, endCol));
+                    Threads.Add(T);
+                    T.Start();
+                }
+            }
+            for (int i = 0; i < Threads.Count; i++) Threads[i].Join();
+            for (int i = 0; i < pixOut.GetLength(0); i++) for (int j = 0; j < pixOut.GetLength(1); j++) OutPic.SetPixel(j, i, pixOut[i, j]);
+            Invoke(new Action(() => { pictureBoxOut.Image = OutPic; }));
+        }
+
+        private void MedianFilterOnRectangle(int edge, int stRow, int stCol, int endRow, int endCol)
+        {
+            int CellLen = edge * edge;
+            int m = CellLen / 2, n = edge / 2;
+            for (int i = stRow; i <= endRow; i++)
+            {
+                for (int j = stCol; j <= endCol; j++)
+                {
+                    int[] R = new int[CellLen], G = new int[CellLen], B = new int[CellLen];
+                    for (int k = i - n; k <= i + n; k++) for (int l = j - n; l <= j + n; l++) { int p = (k - i + n) * edge + l - j + n; R[p] = pixIn[k, l].R; G[p] = pixIn[k, l].G; B[p] = pixIn[k, l].B; }
+                    Array.Sort(R); Array.Sort(G); Array.Sort(B);
+                    pixOut[i,j] = Color.FromArgb(R[m], G[m], B[m]);
+                }
+            }
+        }
+    }
+}
